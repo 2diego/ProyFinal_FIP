70ff2c0c1555396cba445a3657342b733032e965
alexis urbina <alexisurbinaa435@gmail.com>
2025-12-12 17:55:00 -0300

    cambio productos

--- frontend/src/pages/productos/Productos.jsx
+++ frontend/src/pages/productos/Productos.jsx
@@ -4,18 +4,18 @@ import Cards from "../../components/card-prod/Cards";
 import Footer from "../../components/footer/Footer";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import CarritoService from "../../services/carrito.service";
+
 const Productos = () => {
   const location = useLocation();
 
-  useEffect(async () => {
+  useEffect(() => {
     const params = new URLSearchParams(location.search);
     const status = params.get("status");
 
-    const carritoId = localStorage.getItem("carritoId");
+    
     if (status === "approved") {
       localStorage.removeItem("carrito");
-      await CarritoService.deleteCarrito(carritoId);
+      // localStorage.removeItem("carritoId");
       window.history.replaceState({}, "", "/productos");
     }
   }, [location]);
