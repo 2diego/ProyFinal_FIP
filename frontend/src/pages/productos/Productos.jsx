b98089058ba3f677363282a5455a0baf20c7689b
alexis urbina <alexisurbinaa435@gmail.com>
2025-12-12 17:45:37 -0300

    delete carrito

--- frontend/src/pages/productos/Productos.jsx
+++ frontend/src/pages/productos/Productos.jsx
@@ -4,18 +4,18 @@ import Cards from "../../components/card-prod/Cards";
 import Footer from "../../components/footer/Footer";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
+import CarritoService from "../../services/carrito.service";
 const Productos = () => {
   const location = useLocation();
 
-  useEffect(() => {
+  useEffect(async () => {
     const params = new URLSearchParams(location.search);
     const status = params.get("status");
 
-    
+    const carritoId = localStorage.getItem("carritoId");
     if (status === "approved") {
       localStorage.removeItem("carrito");
-      localStorage.removeItem("carritoId");
+      await CarritoService.deleteCarrito(carritoId);
       window.history.replaceState({}, "", "/productos");
     }
   }, [location]);
